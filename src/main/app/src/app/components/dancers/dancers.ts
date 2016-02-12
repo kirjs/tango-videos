bfd76e62782657b5c320f5322fbee188aa8cd70f
Kirill Cherkashin <kirill@kcherkashin.org>
2016-02-11 21:48:59 -0500

    Format dancers nicer

--- src/main/app/src/app/components/dancers/dancers.ts
+++ src/main/app/src/app/components/dancers/dancers.ts
@@ -1,22 +1,25 @@
 import {Component} from 'angular2/core';
 import {DancerService} from "../../services/DancerService";
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
-  selector: 'dancers',
-  template: require('./dancers.html'),
-  styles: [require('./dancers.css')],
-  providers: [],
-  directives: [],
-  pipes: []
+    selector: 'dancers',
+    template: require('./dancers.html'),
+    styles: [require('./dancers.css')],
+    providers: [],
+    directives: [ROUTER_DIRECTIVES],
+    pipes: []
 })
 export class Dancers {
-  dancers:Array = [];
-  fetch(){
-    this.dancerService.list().subscribe((dancers)=> {
-      this.dancers = dancers;
-    });
-  }
-  constructor(private dancerService:DancerService) {
-    this.fetch();
-  }
+    dancers:Array = [];
+
+    fetch() {
+        this.dancerService.list().subscribe((dancers)=> {
+            this.dancers = dancers;
+        });
+    }
+
+    constructor(private dancerService:DancerService) {
+        this.fetch();
+    }
 }
