a83336524009bc3483758797efbb59f655054838
Kirill Cherkashin <kirill@kcherkashin.org>
2016-02-13 17:23:41 -0500

    Add a video player component

---
+++ src/main/app/src/app/components/video-player/video-player.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'video-player',
+    template: require('./video-player.html'),
+    styles: [require('./video-player.css')],
+    providers: [],
+    directives: [],
+    pipes: []
+})
+export class VideoPlayer {
+
+    constructor() {
+    }
+
+}
