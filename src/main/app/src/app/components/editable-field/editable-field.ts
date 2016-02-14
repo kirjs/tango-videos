4f10cf27e207008805b07de45a9d4a5d5acd9752
Kirill Cherkashin <kirill@kcherkashin.org>
2016-02-13 22:27:16 -0500

    #ui #editable-field update value with enter

--- src/main/app/src/app/components/editable-field/editable-field.ts
+++ src/main/app/src/app/components/editable-field/editable-field.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter} from 'angular2/core';
+import {Component, Input, Output, EventEmitter, ViewChild} from 'angular2/core';
 import {NgAutocompleteContainer} from "../autocomplete/ng2-autocomplete";
 import {NgAutocompleteInput} from "../autocomplete/ng2-autocomplete";
 
@@ -16,8 +16,15 @@ export class EditableField {
     @Input() emptyValue:String = '';
     @Input() autocompleteSource:Array<String> = [];
     @Output() update = new EventEmitter();
+    @ViewChild("input") input;
 
     editMode:boolean = false;
+
+    handleKeyup(event){
+        if(event.keyCode == 13){
+            this.changeValue(event.target.value);
+        }
+    }
 
     changeValue(value:string) {
         this.update.emit(value);
