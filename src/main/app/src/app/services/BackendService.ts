c5063056399185b139c87fdc11edb01da3254dd5
Kirill Cherkashin <kirill@kcherkashin.org>
2016-02-14 21:20:39 -0500

    #ui #http #backend-service implement writing

--- src/main/app/src/app/services/BackendService.ts
+++ src/main/app/src/app/services/BackendService.ts
@@ -1,13 +1,26 @@
-import {Http, Response} from 'angular2/http';
+import {Http, Response, Headers} from 'angular2/http';
 import {Injectable} from "angular2/core";
 var YouTubePlayer = require('youtube-player');
 
+interface Dictionary {
+    [index: string]: string;
+}
 @Injectable()
 export class BackendService {
     base:string = 'api/';
+
     read(url: string){
         return this.http.get(this.base + url)
             .map((res) => res.json());
+    }
+
+    write(url: string, params: Dictionary){
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+
+        return this.http.post('api/login/', JSON.stringify(params), {headers: headers})
+            .map(res => res.json());
     }
     constructor(private http:Http){}
 }
