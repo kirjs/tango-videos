93786358e9275254e4a6246b6c03fc241d4763b7
Kirill Cherkashin <kirill@kcherkashin.org>
2016-02-15 14:05:32 -0500

    #ui #http Create a progress observer

--- src/main/app/src/app/services/BackendService.ts
+++ src/main/app/src/app/services/BackendService.ts
@@ -1,10 +1,11 @@
 import {Http, Response, Headers} from 'angular2/http';
 import {Injectable} from "angular2/core";
-import {Observable} from "rxjs";
+import {Observable, Subject} from "rxjs";
 
 @Injectable()
 export class BackendService {
     private base:string = 'api/';
+    public progress;
 
     read(url:string):Observable<any> {
         return this.http.get(this.base + url)
@@ -15,11 +16,24 @@ export class BackendService {
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
         headers.append('Accept', 'application/json');
+        var requestProgress = new Subject().startWith('Starting a request');
+        this.progress.next(requestProgress);
+
 
         return this.http.post(this.base + url, JSON.stringify(params), {headers: headers})
-            .map(res => res.json());
+            .map(res => res.json())
+            .retryWhen(attempts => attempts.flatMap(()=> {
+                return Observable.create((obs)=> {
+                    if (prompt("startAgain?")) {
+                        obs.next();
+                    } else {
+                        obs.complete();
+                    }
+                })
+            }))
     }
 
     constructor(private http:Http) {
+        this.progress = new Subject();
     }
 }
