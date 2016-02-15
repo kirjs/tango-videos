79283d4e30ae6e4fb9d369493d2d80cd9667e73f
Kirill Cherkashin <kirill@kcherkashin.org>
2016-02-15 14:13:22 -0500

    #ui #http Add a basic requestStatus component

---
+++ src/main/app/src/app/components/request-status/request-status.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'request-status',
+    template: require('./request-status.html'),
+    styles: [require('./request-status.css')],
+    providers: [],
+    directives: [],
+    pipes: []
+})
+export class RequestStatus {
+
+    constructor() {
+    }
+
+}
