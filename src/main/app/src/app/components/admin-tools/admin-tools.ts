0999d2dc59c327e1ca02102a6990a42de52c66a1
Kirill Cherkashin <kirill@kcherkashin.org>
2016-02-16 19:37:31 -0500

    #ui #admin-tools Implement rename dancer from

--- src/main/app/src/app/components/admin-tools/admin-tools.ts
+++ src/main/app/src/app/components/admin-tools/admin-tools.ts
@@ -1,16 +1,26 @@
 import {Component} from 'angular2/core';
+import {NeedsPermission} from "../needs-permission/needs-permission";
+import {AdminToolsService} from "../../services/AdminToolsService";
 
 @Component({
     selector: 'admin-tools',
     template: require('./admin-tools.html'),
     styles: [require('./admin-tools.css')],
-    providers: [],
-    directives: [],
+    providers: [AdminToolsService],
+    directives: [NeedsPermission],
     pipes: []
 })
 export class AdminTools {
+    constructor(private adminToolsService:AdminToolsService) {
 
-    constructor() {
     }
 
+    renameDancerStatus:String = '';
+
+    renameDancer(oldName, newName){
+        this.renameDancerStatus = 'In progress';
+        this.adminToolsService.renameDancer(oldName, newName).subscribe(()=>{
+            this.renameDancerStatus = 'Done renaming ' + oldName + ' to ' + newName;
+        });
+    }
 }
---
+++ src/main/app/src/app/services/AdminToolsService.ts
@@ -0,0 +1,14 @@
+import {Injectable} from 'angular2/core';
+import {BackendService} from "./BackendService";
+import {Observable} from "rxjs";
+
+
+@Injectable()
+export class AdminToolsService {
+    constructor(private backendService:BackendService) {
+    }
+
+    renameDancer(oldName:String, newName:String):Observable<any> {
+        return this.backendService.write('adminTools/renameDancer', {oldName, newName});
+    }
+}
