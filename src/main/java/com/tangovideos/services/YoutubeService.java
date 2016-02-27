fd77c07a0a0a6d38e48268cf548331b6c6a46653
Kirill Cherkashin <kirill@kcherkashin.org>
2016-02-26 22:55:42 -0500

    #server Implement video Fetching

--- src/main/java/com/tangovideos/services/YoutubeService.java
+++ src/main/java/com/tangovideos/services/YoutubeService.java
@@ -11,25 +11,49 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 public class YoutubeService {
     final String BASE_URL = "https://www.googleapis.com/youtube/v3/";
     final String API_TOKEN = "AIzaSyCW22iBpph-9DMs3rpHa3iXZDpTV0qsLCU";
 
-    public List<Video> fetchChannelVideos(String id){
+    public List<Video> fetchChannelVideos(String id, long startingFrom) {
+        return fetchChannelVideos(id, startingFrom, "");
+    }
+
+    public List<Video> fetchChannelVideos(String id, long startingFrom, String pageToken) {
+        final ArrayList<Video> result = Lists.newArrayList();
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target(BASE_URL).path("playlistItems")
                 .queryParam("key", API_TOKEN)
                 .queryParam("playlistId", id)
+                .queryParam("maxResults", 50)
+                .queryParam("pageToken", pageToken)
                 .queryParam("part", "snippet");
 
         String response = target.request("application/json").get(new GenericType<>(String.class));
+        final JSONObject jsonResponse = new JSONObject(response);
+        final JSONArray items = jsonResponse.getJSONArray("items");
+        for (int i = 0; i < items.length(); i++) {
+            final JSONObject snippet = items.getJSONObject(i).getJSONObject("snippet");
+            final long publishedAt = Instant.parse(snippet.getString("publishedAt")).getEpochSecond();
+            if (publishedAt <= startingFrom) {
+                return result;
+            }
 
+            final String videoId = snippet.getJSONObject("resourceId").getString("videoId");
+            result.add(snippetToVideo(videoId, snippet));
+        }
+        final String nextPageToken = jsonResponse.optString("nextPageToken");
+        if (!nextPageToken.isEmpty()) {
+            result.addAll(fetchChannelVideos(id, startingFrom, nextPageToken));
+        }
 
-
-        return Lists.newArrayList();
+        return result;
     }
+
     public Video getVideoInfo(String id) {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target(BASE_URL).path("videos")
@@ -45,19 +69,23 @@ public class YoutubeService {
                     .getJSONObject(0)
                     .getJSONObject("snippet");
 
-            final Video result = new Video(
-                    id,
-                    snippet.getString("title"),
-                    snippet.getString("publishedAt")
-            );
-            result.setDescription(snippet.getString("description"));
-            result.setDancers(BasicNameParser.extractNames(result.getTitle(), result.getDescription()));
-            return result;
+            return snippetToVideo(id, snippet);
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
         return null;
+    }
+
+    private Video snippetToVideo(String id, JSONObject snippet) {
+        final Video result = new Video(
+                id,
+                snippet.getString("title"),
+                snippet.getString("publishedAt")
+        );
+        result.setDescription(snippet.getString("description"));
+        result.setDancers(BasicNameParser.extractNames(result.getTitle(), result.getDescription()));
+        return result;
     }
 
     public Channel getChannelInfoById(String id) {
--- src/test/com/tangovideos/services/YoutubeServiceTest.java
+++ src/test/com/tangovideos/services/YoutubeServiceTest.java
@@ -5,6 +5,7 @@ import com.tangovideos.models.Video;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.time.Instant;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -27,8 +28,20 @@ public class YoutubeServiceTest {
 
     @Ignore @Test
     public void testGetPlaylistVideos() throws Exception {
-        final List<Video> videos = youtubeService.fetchChannelVideos("UUgWXMx-Pu9QcW0zepRecnqw");
+        final List<Video> videos = youtubeService.fetchChannelVideos("UUgWXMx-Pu9QcW0zepRecnqw", 0);
+        assertEquals(videos.size(), 17);
+
+        final List<Video> videosAfterOct18 = youtubeService.fetchChannelVideos("UUgWXMx-Pu9QcW0zepRecnqw",
+                Instant.parse("2015-10-17T00:00:00.000Z").getEpochSecond());
+        assertEquals(videosAfterOct18.size(), 6);
+
+        final List<Video> videosAfterTheSecondToLast = youtubeService.fetchChannelVideos("UUgWXMx-Pu9QcW0zepRecnqw", 1455840400);
+        assertEquals(videosAfterTheSecondToLast.size(), 1);
     }
 
+    @Test
+    public void  testTime(){
+        final long time1 = Instant.parse("2015-05-31T17:50:32.000Z").getEpochSecond();
+    }
 
 }
