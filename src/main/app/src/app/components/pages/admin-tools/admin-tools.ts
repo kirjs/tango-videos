cb5c54bb72552b63fc7d8c2b08cb314fe922c20d
Kirill Cherkashin <kirill@kcherkashin.org>
2016-02-27 19:55:04 -0500

    #ui #admin-tools Display autocomplete for dancers names

--- src/main/app/src/app/components/pages/admin-tools/admin-tools.ts
+++ src/main/app/src/app/components/pages/admin-tools/admin-tools.ts
@@ -3,6 +3,9 @@ import {Observable} from 'rxjs';
 import {AdminToolsService} from "../../../services/AdminToolsService";
 import {NeedsPermission} from "../../common/needs-permission/needs-permission";
 import {ChannelManager} from "./channel-manager/channel-manager";
+import {DancerService} from "../../../services/DancerService";
+import {NgAutocompleteContainer} from "../../common/autocomplete/ng2-autocomplete";
+import {NgAutocompleteInput} from "../../common/autocomplete/ng2-autocomplete";
 
 export interface Stat {
     name: string;
@@ -14,15 +17,20 @@ export interface Stat {
     template: require('./admin-tools.html'),
     styles: [require('./admin-tools.css')],
     providers: [AdminToolsService],
-    directives: [NeedsPermission, ChannelManager],
+    directives: [NeedsPermission, ChannelManager, NgAutocompleteContainer, NgAutocompleteInput],
     pipes: []
 })
 export class AdminTools {
 
     stats$:Observable<Array<Stat>>;
+    private dancersSource;
 
-    constructor(private adminToolsService:AdminToolsService) {
+
+    constructor(private adminToolsService:AdminToolsService, dancers:DancerService) {
         this.stats$ = this.adminToolsService.stats();
+        this.dancersSource = dancers.listNames().map(result=> {
+            return result.map((dancer)=> dancer.name);
+        });
     }
 
     renameDancerStatus:String = '';
@@ -30,7 +38,7 @@ export class AdminTools {
     renameDancer(oldName, newName) {
         this.renameDancerStatus = 'In progress';
         this.adminToolsService.renameDancer(oldName, newName).subscribe((count)=> {
-            this.renameDancerStatus = 'Done renaming ' + oldName + ' to ' + newName + '. ['+count+' instances]';
+            this.renameDancerStatus = 'Done renaming ' + oldName + ' to ' + newName + '. [' + count + ' instances]';
         });
     }
 }
