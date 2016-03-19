2f6c10b1984afcce63d09b34f54d4e4db125d426
Kirill Cherkashin <kirill@kcherkashin.org>
2016-03-19 18:01:49 -0400

    #server #dancers Add a simple neo4j query runner method

--- src/main/java/com/tangovideos/services/neo4j/Neo4DB.java
+++ src/main/java/com/tangovideos/services/neo4j/Neo4DB.java
@@ -1,6 +1,9 @@
 package com.tangovideos.services.neo4j;
 
+import com.google.common.collect.ImmutableMap;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Result;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 import java.io.File;
@@ -10,7 +13,20 @@ public class Neo4DB {
     public final GraphDatabaseService graphDb;
 
     public Neo4DB() {
-        final File storeDir = new File(DB_PATH);
+        final File storeDir = new File("/usr/local/tomcat/bin/neo4jDb12");
         graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(storeDir);
+    }
+
+    public static void main(String[] args){
+        final GraphDatabaseService graphDb = new Neo4DB().graphDb;
+        final String query = "MATCH (d:Dancer {id: 'Carlos Espinoza'})--(n) " +
+                "WHERE NOT n:Video " +
+                "RETURN d, labels(n)    ";
+        final ImmutableMap<String, Object> params = ImmutableMap.of();
+
+        try (Transaction tx = graphDb.beginTx(); Result result = graphDb.execute(query, params)) {
+            System.out.println(result.resultAsString());
+            tx.success();
+        }
     }
 }
