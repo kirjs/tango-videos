513c75ba562623565758611761f0f53590938f99
Kirill Cherkashin <kirill@kcherkashin.org>
2016-03-20 15:47:16 -0400

    #ui clean up, formatting

--- src/main/app/src/app/components/entities/video/video-tile/video-tile.ts
+++ src/main/app/src/app/components/entities/video/video-tile/video-tile.ts
@@ -14,21 +14,23 @@ import {Video} from "../../../../interfaces/video";
     pipes: []
 })
 export class VideoTile {
-    @Input() video: Video;
-    @Input() readonly: boolean = true;
+    @Input() video:Video;
+    @Input() readonly:boolean = true;
     @Output() play:EventEmitter<any>;
+
     constructor() {
         this.play = new EventEmitter();
     }
 
-    playVideo(){
+    playVideo() {
         this.play.emit(this.video);
     }
-    getGenre(){
-        if(!this.video.songs || !this.video.songs[0] ){
+
+    getGenre() {
+        if (!this.video.songs || !this.video.songs[0]) {
             return '';
         }
-        return this.video.songs[0].genre.toLowerCase() || '';
+        return this.video.songs[0].genre && this.video.songs[0].genre.toLowerCase() || '';
     }
 
     getThumbnail(id:string) {
