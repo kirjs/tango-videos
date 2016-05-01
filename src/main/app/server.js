31957d2000187da0227fc3d3e12655c15ae642c6
Kirill Cherkashin <kirill@kcherkashin.org>
2016-05-01 17:50:22 -0400

    #server #ui take port and proxy url in the node server

--- src/main/app/server.js
+++ src/main/app/server.js
@@ -2,6 +2,9 @@ var WebpackDevServer = require("webpack-dev-server");
 var webpack = require("webpack");
 var compiler = webpack(require('./webpack.config.js'));
 
+var apiProxy = process.env.TANGO_VIDEOS_REST || 'http://localhost:8087/';
+var port = process.env.TANGO_VIDEOS_PORT || '8084';
+
 var server = new WebpackDevServer(compiler, {
     inline: true,
     colors: true,
@@ -13,12 +16,12 @@ var server = new WebpackDevServer(compiler, {
     historyApiFallback: true,
     proxy: {
         '/api/*': {
-            target: 'http://localhost:8087/'
+            target: apiProxy
         }
     }
 });
 
-var port = 8084;
+
 server.listen(port, "0.0.0.0", function () {
     console.log("Running on " + port);
 });
