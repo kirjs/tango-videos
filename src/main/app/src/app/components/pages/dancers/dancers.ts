057e8d9bd1ad8f1b1627231e032b781a2ae73fe1
Kirill Cherkashin <kirill@kcherkashin.org>
2016-07-17 17:48:40 -0400

    #style Use material card for the list of dancers

--- src/main/app/src/app/components/pages/dancers/dancers.ts
+++ src/main/app/src/app/components/pages/dancers/dancers.ts
@@ -3,12 +3,16 @@ import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 import {DancerService} from "../../../services/DancerService";
 
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
+import {MD_GRID_LIST_DIRECTIVES} from '@angular2-material/grid-list';
+
 @Component({
     selector: 'dancers',
     template: require('./dancers.html'),
     styles: [require('./dancers.css')],
     providers: [],
-    directives: [ROUTER_DIRECTIVES],
+    directives: [ROUTER_DIRECTIVES, MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_GRID_LIST_DIRECTIVES],
     pipes: []
 })
 export class Dancers {
